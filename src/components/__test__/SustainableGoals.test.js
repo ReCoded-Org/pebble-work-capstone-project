c51746f80f2b2ad328db34f4fddac1f25eee9997
DKatuk <dilarakatuk@gmail.com>
2022-07-20 21:43:04 +0300

    test: implemented-SustaiableGoals-component-snapshot-test

---
+++ src/components/__test__/SustainableGoals.test.js
@@ -0,0 +1,7 @@
+import renderer from "react-test-renderer";
+import SustainableGoals from "../SustainableGoals";
+
+it("renders correctly", () => {
+    const tree = renderer.create(<SustainableGoals />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
