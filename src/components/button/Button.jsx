60da2f9bdd9fd7801d65414f76fb1ade2710788f
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-07-26 00:28:27 +0300

    feat(button): creating a custimized button component
    we have to use a div as Button wrapper so we can change our buttons styling, please pay inteantion
    to the comment i made in side the c  omponent
    
    feat #18

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Button = ({ type = "button", onClick, children }) => {
+    //you should put these classes to the div you will use to wrape the button so you can use it to change buttons styles
+    // <div className='w-max rounded ...'>
+
+    return (
+        <>
+            <button
+                type={type}
+                onClick={onClick}
+                className='rounded border py-2 px-4 text-white'
+            >
+                {children}
+            </button>
+        </>
+    );
+};
+
+export default Button;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 
 import Footer from "./Footer";
+import Button from "../../components/button/Button";
 
 export default function Layout({ children }) {
     // Put Header or Footer around the children element
+
     return (
         <>
             {children}
+            <div className='w-max rounded border-sky-700 bg-secondary-300  hover:bg-sky-700'>
+                <Button>Get started</Button>
+            </div>
             <Footer />
         </>
     );
