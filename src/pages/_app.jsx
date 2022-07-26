102d774e422889f8b05ea4b3da86add4da746499
oznuren <95101906+oznuren@users.noreply.github.com>
2022-07-26 19:23:38 +0300

    fix(howitworks): increased the spacing between cards

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,15 @@
 import { appWithTranslation } from "next-i18next";
 
 import "@/styles/globals.css";
+import HowItWorks from "@/components/HowItWorks";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <div>
+            <Component {...pageProps} />
+            <HowItWorks />
+        </div>
+    );
 }
 
 export default appWithTranslation(MyApp);
