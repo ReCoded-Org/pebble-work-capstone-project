14690bcfee72185cf4df188abda1101108943727
oznuren <95101906+oznuren@users.noreply.github.com>
2022-07-26 15:50:04 +0300

    fix(howitworks): fixed the responsiveness
    #4

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -10,11 +10,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function HowItWorks() {
     return (
         <div className='how-it-works'>
-            <h1 className='text-l mb-3 flex justify-center font-bold sm:mb-5 sm:text-xl md:mb-7 md:text-2xl lg:mb-9 lg:text-4xl'>
+            <h1 className='mt-5 flex justify-center text-xl font-bold md:m-3 md:text-4xl lg:mb-9 lg:text-4xl'>
                 How It Works
             </h1>
-            <div className='flex items-center justify-center'>
-                <div className='w-30 m-1 overflow-hidden rounded sm:m-2 md:m-4 md:w-52 lg:m-8 lg:w-80'>
+            <div className='flex flex-col items-center justify-center lg:flex-row'>
+                <div className='w-30 m-4 overflow-hidden rounded md:m-4 md:w-4/5 lg:m-8 lg:w-80'>
                     <Image
                         class='w-full'
                         src='/images/register.png'
@@ -24,16 +24,16 @@ function HowItWorks() {
                         layout='responsive'
                     />
                     <div className='px-6 py-4'>
-                        <div className='md:text-md mb-2 flex justify-center text-center text-[8px] font-bold sm:text-sm lg:text-lg'>
+                        <div className='m-2 flex justify-center text-center font-bold md:text-3xl lg:text-lg'>
                             Register
                         </div>
-                        <p className='md:text-md text-center  text-[6px] text-gray-700 sm:text-sm lg:text-lg'>
+                        <p className='text-center text-sm text-gray-700 md:text-xl lg:text-lg'>
                             Body placeholder for text paragraph, a paragraph is
                             a self-contained unit of text description.
                         </p>
                     </div>
-                    <div className=' px-6 pt-4 pb-2'>
-                        <h1 className='title md:text-md text-center text-[8px] font-bold sm:text-sm lg:text-lg'>
+                    <div className='mb-4 p-2 px-6 pt-4'>
+                        <h1 className='title text-center font-bold md:text-2xl lg:text-lg'>
                             {" "}
                             <Link href='/posts/first-post'>
                                 <a>
@@ -44,7 +44,7 @@ function HowItWorks() {
                         </h1>
                     </div>
                 </div>
-                <div className='w-30 m-1 overflow-hidden rounded sm:m-2 md:m-4 md:w-52 lg:m-8 lg:w-80'>
+                <div className='w-30 m-4 overflow-hidden rounded md:m-4 md:w-4/5 lg:m-8 lg:w-80'>
                     <Image
                         class='w-full'
                         src='/images/attend.png'
@@ -54,16 +54,16 @@ function HowItWorks() {
                         layout='responsive'
                     />
                     <div className='px-6 py-4'>
-                        <div className='md:text-md mb-2 flex justify-center text-center text-[8px] font-bold sm:text-sm lg:text-lg'>
+                        <div className='m-2 flex justify-center text-center font-bold md:text-3xl lg:text-lg'>
                             Attend events
                         </div>
-                        <p className='md:text-md text-center  text-[6px] text-gray-700 sm:text-sm lg:text-lg'>
+                        <p className='text-center text-sm text-gray-700 md:text-xl lg:text-lg'>
                             Body placeholder for text paragraph, a paragraph is
                             a self-contained unit of text description.
                         </p>
                     </div>
-                    <div className=' px-6 pt-4 pb-2'>
-                        <h1 className='title md:text-md text-center text-[8px] font-bold sm:text-sm lg:text-lg'>
+                    <div className='mb-4 p-2 px-6 pt-4'>
+                        <h1 className='title text-center font-bold md:text-2xl lg:text-lg'>
                             {" "}
                             <Link href='/posts/first-post'>
                                 <a>
@@ -74,7 +74,7 @@ function HowItWorks() {
                         </h1>
                     </div>
                 </div>
-                <div className='w-30 m-1 overflow-hidden rounded sm:m-2 md:m-4 md:w-52 lg:m-8 lg:w-80'>
+                <div className='w-30 m-4 overflow-hidden rounded md:m-4 md:w-4/5 lg:m-8 lg:w-80'>
                     <Image
                         class='w-full'
                         src='/images/organize.png'
@@ -84,16 +84,16 @@ function HowItWorks() {
                         layout='responsive'
                     />
                     <div className='px-6 py-4'>
-                        <div className='md:text-md mb-2 flex justify-center text-center text-[8px] font-bold sm:text-sm lg:text-lg'>
+                        <div className='m-2 flex justify-center text-center font-bold md:text-3xl lg:text-lg'>
                             Organize on your own!
                         </div>
-                        <p className='md:text-md text-center text-[6px] text-gray-700 sm:text-sm lg:text-lg'>
+                        <p className='text-center text-sm text-gray-700 md:text-xl lg:text-lg'>
                             Body placeholder for text paragraph, a paragraph is
                             a self-contained unit of text description.
                         </p>
                     </div>
-                    <div className='px-6 pt-2 pb-2 sm:pt-4 md:pt-4 lg:pt-4'>
-                        <h1 className='title md:text-md text-center text-[8px] font-bold sm:text-sm lg:text-lg'>
+                    <div className='mb-4 p-2 px-6 pt-4'>
+                        <h1 className='title text-center font-bold md:text-2xl lg:text-lg'>
                             {" "}
                             <Link href='/posts/first-post'>
                                 <a>
