81aa687b7ced62c17a19665803e1e024fd38accb
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-07-29 00:32:35 +0300

    feat(signup): adding sign up component with a titl props

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,11 @@
 import * as React from "react";
 
 import Footer from "./Footer";
-import Signin from "../Signinup/__test__/Signin";
-import Signup from "../Signinup/__test__/SignUp";
 export default function Layout({ children }) {
     // Put Header or Footer around the children element
 
     return (
         <>
-            <Signup />
-            <Signin />
             {children}
             <Footer />
         </>
---
+++ src/components/signinup/__test__/Signup.test.jsx
@@ -0,0 +1,10 @@
+"use strict";
+
+import renderer from "react-test-renderer";
+
+import Signup from "../Signup";
+
+it("renders correctly", () => {
+    const tree = renderer.create(<Signup />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
