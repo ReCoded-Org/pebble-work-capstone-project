1a3c9360e72dff22d7ff8fd82ceea86133c5019e
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-07-29 12:34:11 +0300

    test(button): editing button.test.jsx

--- src/components/__test__/button.test.jsx
+++ src/components/__test__/button.test.jsx
@@ -5,6 +5,17 @@ import renderer from "react-test-renderer";
 import Button from "../Button";
 
 it("renders correctly", () => {
-    const tree = renderer.create(<Button />).toJSON();
+    const tree = renderer
+        .create(
+            <Button
+                label='Get started'
+                bgColor='bg-secondary-300'
+                textColor='text-white'
+                fontWeight='font-normal'
+                height='h-8'
+                width='w-30'
+            />
+        )
+        .toJSON();
     expect(tree).toMatchSnapshot();
 });
