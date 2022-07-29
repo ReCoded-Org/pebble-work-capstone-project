3603dd8ad5c959674a381cd874261386b03f339f
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-07-29 16:37:55 +0300

    refactor(sign up): deleting unnecassery code in layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 
 import Footer from "./Footer";
-import Signup from "../signinup/Signup";
 export default function Layout({ children }) {
     // Put Header or Footer around the children element
 
     return (
         <>
-            <Signup title='Sign in' />
             {children}
             <Footer />
         </>
