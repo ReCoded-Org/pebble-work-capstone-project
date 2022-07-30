28cee13d55d77fc292dda29b950ec0a431eb4cb4
oznuren <95101906+oznuren@users.noreply.github.com>
2022-07-30 13:05:41 +0300

    feat(inputcomponent): added the label prop and tag, modified props according to needs
    Added the name, className, label prop
    
    #37

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -1,12 +1,22 @@
-const InputComponent = (props) => {
-    const { placeholder = "Your input here", type = "text", callback } = props;
+const InputComponent = ({
+    placeholder = "Your input here",
+    type = "text",
+    callback,
+    className = "h-full w-full rounded-md border-2 border-black p-2",
+    label,
+    name,
+}) => {
     return (
-        <input
-            className={`h-full w-full rounded-md border-2 border-black p-2`}
-            type={type}
-            placeholder={placeholder}
-            onChange={callback}
-        />
+        <>
+            <input
+                className={className}
+                type={type}
+                placeholder={placeholder}
+                onChange={callback}
+                name={name}
+            />
+            <label htmlFor={name}>{label}</label>
+        </>
     );
 };
 
