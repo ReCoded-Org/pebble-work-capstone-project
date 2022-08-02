3b0eab5dab9a99632bbb9b2e717a9e354d12aad7
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-08-03 01:10:15 +0300

    feat(create event): adding terms section

--- src/components/eventCreation/ChooseLocationAndEventType.jsx
+++ src/components/eventCreation/ChooseLocationAndEventType.jsx
@@ -7,7 +7,7 @@ const ChooseLocationAndEventType = () => {
     const [Location, setLocation] = useState("Which city?");
     return (
         <>
-            <div className='mx-8 mt-3 mb-4'>
+            <div className='mx-8 mt-3 mb-4 divide-y'>
                 <div className='mb-4 flex   flex-col gap-10 py-3 md:flex-row md:justify-between'>
                     <div>
                         <h1 className='text-3xl font-bold text-black'>
@@ -57,7 +57,7 @@ const ChooseLocationAndEventType = () => {
                         <h1 className='text-3xl font-bold text-black'>
                             Second, Choose event type:
                         </h1>
-                        <p className='my-1  w-full text-gray-500 '>
+                        <p className='my-1  text-gray-500 '>
                             Every pebble event should serve at least one of the
                             sustainable development goals of United Nations.
                             Which goal do you want to help in? Select all that
@@ -174,12 +174,12 @@ const ChooseLocationAndEventType = () => {
                         <h1 className='text-3xl font-bold text-black'>
                             Event title:
                         </h1>
-                        <p className='my-1 w-full text-gray-500'>
+                        <p className='my-1 text-gray-500 md:w-full'>
                             Choose a title that will give people a clear idea of
                             what the event is about. Feel free to be creative!
                             You canedit this later if you change your mind.
                         </p>
-                        <div className='relative w-96  py-5'>
+                        <div className=' w-full py-5 '>
                             <InputComponent
                                 placeholder='Izmir Clean Energy Brainstorm meeting'
                                 className='w-full rounded border border-secondary-300 px-2 py-1 shadow hover:border-primary-200 focus:outline-none'
@@ -187,6 +187,7 @@ const ChooseLocationAndEventType = () => {
                         </div>
                     </div>
                 </div>
+
                 <div className='mb-4 flex   flex-col gap-10 py-3 md:flex-row md:justify-between'>
                     <div>
                         <h1 className='text-3xl font-bold text-black'>
@@ -196,15 +197,16 @@ const ChooseLocationAndEventType = () => {
                             Describe the purpose of your event. Who should join
                             and what will you do at the event?
                         </p>
-                        <div className='relative w-96  py-5'>
-                            <InputComponent
+                        <div className=' w-full  py-3'>
+                            <textarea
                                 placeholder='Please write at least 50 characters'
-                                className='h-60 w-full rounded border border-secondary-300 px-2 py-1 shadow hover:border-primary-200 focus:outline-none'
+                                className='h-60 w-full rounded border border-secondary-300 px-2 shadow focus:outline-none'
                             />
                         </div>
                     </div>
                 </div>
-                <div className='mb-4 flex   flex-col gap-10 py-3 md:flex-row md:justify-between'>
+
+                <div className='mb-4 flex flex-col gap-10 py-3 md:w-full md:flex-row md:justify-between'>
                     <div>
                         <h1 className='text-3xl font-bold text-black'>
                             Event Image:
@@ -213,19 +215,49 @@ const ChooseLocationAndEventType = () => {
                             We have found that listings with a photo attract
                             more interest.
                         </p>
-                        <div className='relative w-96  py-5'>
-                            <div className='w-25 pointer-events-none absolute inset-y-0 left-0 flex items-center hover:border-primary-200'>
-                                <button className='w-full rounded border-2 border-r-4 border-secondary-300 px-1 '>
+                        <div className='relative py-5  md:w-full'>
+                            <div className='w-25  absolute inset-y-0 left-0 flex items-center hover:border-primary-200'>
+                                <button className='h-8 w-full cursor-pointer rounded border-2 border-r-4 border-secondary-300 px-1 hover:bg-primary-200 '>
                                     Browse
                                 </button>
                             </div>
-                            <InputComponent
-                                placeholder=' '
-                                className='w-full rounded border border-secondary-300 px-2  shadow  focus:outline-none'
-                            />
+                            <input className=' h-8 w-full rounded border border-secondary-300 focus:outline-none '></input>
                         </div>
                     </div>
                 </div>
+                <div className='mb-3 flex   flex-col gap-10 py-3 md:flex-row md:justify-between'>
+                    <div>
+                        <h1 className='text-3xl font-bold text-black'>
+                            Almost Done! Just take a minute to review our
+                            guidlines.
+                        </h1>
+                        <p className='my-1 w-full text-gray-500'>
+                            Pebble is all about helping people with the help of
+                            volunteers like you. This means that all events
+                            should:
+                        </p>{" "}
+                        <p className=' my-1 w-full pl-1 text-black'>
+                            - Be transparent about the event’s intentions.
+                            <br></br>- Encourage real human interactions in
+                            person or online. <br></br>- Have the host present
+                            in all events
+                        </p>
+                        <p className=' my-1 w-full pl-1 text-gray-400'>
+                            You can read more about all of this in our
+                            <a
+                                href='#'
+                                className='p-1 text-blue-600 hover:text-primary-200'
+                            >
+                                Community guidlines
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className='mb-4 flex flex-col items-center  justify-center gap-10 py-3 '>
+                <button className='rounded border-2 border-black py-4 px-4 hover:bg-primary-200'>
+                    Agree with terms and create event!
+                </button>
             </div>
         </>
     );
