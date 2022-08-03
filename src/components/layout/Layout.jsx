e1df192c2cd1e155517b58cb1c55c3b27a0020c9
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-08-03 21:27:24 +0300

    test: correcting test

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -11,6 +11,7 @@ export default function Layout({ children }) {
             <header>
                 <Navbar />
             </header>
+
             {children}
             <Footer />
         </>
