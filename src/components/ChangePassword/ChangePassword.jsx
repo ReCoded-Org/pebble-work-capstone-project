aef547cf0e69ae2d64817b42dffce5522c53a353
oznuren <95101906+oznuren@users.noreply.github.com>
2022-08-07 16:08:29 +0300

    fix(changepassword): fixed the alignment of the contents
    #27

--- src/components/ChangePassword/ChangePassword.jsx
+++ src/components/ChangePassword/ChangePassword.jsx
@@ -3,56 +3,60 @@ import Button from "../Button";
 
 const ChangePassword = () => {
     return (
-        <div className='m-8 h-48 justify-center overflow-hidden rounded-lg bg-secondary-200 md:m-20 md:h-64 lg:mr-[300px] lg:ml-[300px] lg:h-80'>
-            <div className='text-md m-4 mt-6 flex font-medium md:m-6 md:mt-8 md:text-2xl lg:mt-16 lg:ml-12 lg:text-3xl'>
-                <h2>Change Password</h2>
-            </div>
-            <div className='justify-center overflow-hidden lg:ml-8 lg:flex-row lg:flex-wrap lg:items-start lg:justify-start'>
-                <form>
-                    <div className='m-2 flex flex-row justify-center lg:justify-items-center'>
-                        <div className='w-30 m-1 h-8 md:m-2 md:h-12 md:w-64 lg:w-80'>
-                            <InputComponent
-                                type='password'
-                                placeholder='Password'
-                            />
-                        </div>
+        <div className='m-8 flex justify-center'>
+            <div className='align-center h-[200px] w-[400px] justify-center overflow-hidden rounded-lg bg-[#a4e7f4] md:h-[300px] md:w-[600px] lg:h-[450px] lg:w-[900px]'>
+                <div className='m-8 lg:m-24 lg:justify-center'>
+                    <div className='text-md mb-6 flex font-medium md:mb-8 md:mt-14 md:text-2xl lg:mt-16 lg:mb-12 lg:text-3xl'>
+                        <h2>Change Password</h2>
+                    </div>
+                    <div className='justify-center overflow-hidden lg:flex-row lg:flex-wrap lg:items-start lg:justify-start'>
+                        <form>
+                            <div className='flex flex-row justify-center md:justify-between lg:justify-between lg:justify-items-center'>
+                                <div className='w-30 mr-4 h-8 md:h-12 md:w-64 lg:w-80'>
+                                    <InputComponent
+                                        type='password'
+                                        placeholder='Password'
+                                    />
+                                </div>
 
-                        <div className='w-30 m-1 h-8 md:m-2 md:h-12 md:w-64 lg:w-80'>
-                            <InputComponent
-                                type='password'
-                                placeholder='Retype password'
-                            />
-                        </div>
+                                <div className='w-30 h-8 md:h-12 md:w-64 lg:w-80'>
+                                    <InputComponent
+                                        type='password'
+                                        placeholder='Retype password'
+                                    />
+                                </div>
+                            </div>
+                            <div className='mt-6 flex flex-row justify-end md:mt-8 lg:mt-12'>
+                                <div className='mr-2 justify-end md:mr-4 lg:mr-6'>
+                                    <Button
+                                        label='Submit'
+                                        bgColor='bg-primary-200'
+                                        textColor='text-white'
+                                        fontWeight='font-medium'
+                                        width='w-20 md:w-32 lg:w-36'
+                                        height='h-8 md:h-12 lg:h-12'
+                                        borderColor=''
+                                        fontSize='text-[11px] md:text-xl lg:text-xl'
+                                        border=''
+                                    />
+                                </div>
+                                <div className=''>
+                                    <Button
+                                        label='Cancel'
+                                        bgColor='bg-white'
+                                        textColor='text-black'
+                                        fontWeight='font-medium'
+                                        width='w-20 md:w-32 lg:w-36'
+                                        height='h-8 md:h-12 lg:h-12'
+                                        borderColor='border-black'
+                                        fontSize='text-[11px] md:text-xl lg:text-xl'
+                                        border='border border-b-2 border-r-2'
+                                    />
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div className='m-4 flex flex-row justify-end lg:mr-12'>
-                        <div className='mr-2 justify-end md:mr-4 lg:mr-6'>
-                            <Button
-                                label='Submit'
-                                bgColor='bg-primary-200'
-                                textColor='text-white'
-                                fontWeight='font-medium'
-                                width='w-20 md:w-32 lg:w-36'
-                                height='h-8 md:h-12 lg:h-12'
-                                borderColor=''
-                                fontSize='text-[11px] md:text-xl lg:text-xl'
-                                border=''
-                            />
-                        </div>
-                        <div className=''>
-                            <Button
-                                label='Cancel'
-                                bgColor='bg-white'
-                                textColor='text-black'
-                                fontWeight='font-medium'
-                                width='w-20 md:w-32 lg:w-36'
-                                height='h-8 md:h-12 lg:h-12'
-                                borderColor='border-black'
-                                fontSize='text-[11px] md:text-xl lg:text-xl'
-                                border='border border-b-2 border-r-2'
-                            />
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     );
