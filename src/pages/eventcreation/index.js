be108d125ff7bdddc66c6e9b5e06ab349f7e65b2
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-08-08 19:32:49 +0300

    feat: creating a event creation page

---
+++ src/pages/eventcreation/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import EventCreation from '@/components/eventCreation'
+import Layout from '@/components/layout/Layout'
+
+const Eventcreation = () => {
+  return (
+    <div>
+      <Layout>
+        <EventCreation />
+      </Layout></div>
+  )
+}
+
+export default Eventcreation;
