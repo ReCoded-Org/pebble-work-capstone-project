3c72fbf2dd3a7cd19908207c435c1d195bebb316
oznuren <95101906+oznuren@users.noreply.github.com>
2022-08-08 19:28:25 +0300

    feat(editinterests): changed a lot in code not much in looks
    Re-created the buttons by mapping them, added two proprs so that they can be restyled, added the
    click styling, re-ordered the styling. Before it was adjusted for the best design for the buttons to
    fit. now, i added them as they were numbered. Please compare the two and if it looks bad i can
    revert. all reviews are appreciated :)
    
    #26

--- src/components/EditInterests/EditInterests.jsx
+++ src/components/EditInterests/EditInterests.jsx
@@ -1,205 +1,64 @@
 import Button from "../Button";
+import { useState } from "react";
 
-const EditInterests = () => {
-    const clickHandler = (e) => {
-        bgColor = "bg-primary-200";
-        textColor = "text-white";
+const EditInterests = ({ beforeClick, afterClick }) => {
+    const labels = [
+        "No Poverty",
+        "Zero Hunger",
+        "Good Health and Well-Being",
+        "Quality Education",
+        "Gender Equality",
+        "Clean Water and Sanitation",
+        "Affordable and Clean Energy",
+        "Decent Work and Economic Growth",
+        "Industry, Innovation and Infrastructure",
+        "Reduced Inequalities",
+        "Sustainable Cities and Communities",
+        "Responsible Consumption and Production",
+        "Climate Action",
+        "Life Below Water",
+        "Life On Land",
+        "Peace, Justice and Strong Institutions",
+        "Partnerships to achieve the Goal",
+    ];
+    const [selected, setSelected] = useState([]);
+
+    const handleClick = (label) => {
+        setSelected([...selected, label]);
+        if (selected.includes(label)) {
+            setSelected(selected.filter((item) => item !== label));
+        }
     };
+
     return (
         <div className='m-4 mt-12 md:m-12'>
             <h2 className='text-md m-4 font-medium md:m-6 md:text-2xl lg:mt-16 lg:ml-12 lg:text-3xl'>
                 Your Interests
             </h2>
             <div className='flex flex-wrap items-center justify-center overflow-hidden lg:ml-8 lg:flex-row lg:flex-wrap lg:items-start lg:justify-start'>
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='No Poverty'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-20 md:w-44 lg:w-40'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                        onClick={clickHandler}
-                    />
-                </div>
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Industry, Innovation and Infrastructure'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-[400px] lg:w-[400px]'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border  lg:border-2'
-                    />
-                </div>
-
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Quality Education'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-52 lg:w-52'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Sustainable Cities and Communities'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-96 lg:w-96'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
-
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Gender Equality'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-52 lg:w-52'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Affordable and Clean Energy'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-80 lg:w-80'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
-
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Decent Work and Economic Growth'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-96 lg:w-96'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Reduced Inequalities'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-64 lg:w-64'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
-
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Good Health and Well-Being'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-80 lg:w-80'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Clean Water and Sanitation'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-72 lg:w-80'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border  lg:border-2'
-                    />
-                </div>
-
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Responsible Consumption and Production'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-[440px] lg:w-[440px]'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Life Below Water'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-52 lg:w-52'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
-
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Life On Land'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-44 lg:w-52'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
-                <div className='m-1 md:m-2'>
-                    <Button
-                        label='Peace, Justice and Strong Institutions'
-                        bgColor='bg-white'
-                        textColor='text-black'
-                        fontWeight='font-medium'
-                        width='w-30 md:w-[400px] lg:w-[400px]'
-                        height='h-8 md:h-14 lg:h-14'
-                        borderColor='border-black'
-                        fontSize='text-[9px] md:text-xl lg:text-xl'
-                        border='border lg:border-2'
-                    />
-                </div>
+                {labels.map((label) => (
+                    <div key={label} className='m-1 md:m-2 lg:m-3'>
+                        <Button
+                            label={label}
+                            bgColor={
+                                selected.includes(label)
+                                    ? "bg-primary-200"
+                                    : "bg-white"
+                            }
+                            fontWeight='font-medium'
+                            width='w-auto'
+                            height='h-7 md:h-12 lg:h-12'
+                            fontSize='text-[9px] md:text-xl lg:text-lg'
+                            border='border lg:border-2'
+                            customStyle={
+                                selected.includes(label)
+                                    ? `${afterClick}`
+                                    : `${beforeClick}`
+                            }
+                            onClick={() => handleClick(label)}
+                        />
+                    </div>
+                ))}
             </div>
         </div>
     );
