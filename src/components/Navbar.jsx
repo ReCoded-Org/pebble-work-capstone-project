3d6581d6dcf1ce45441b4700179b26712701b0f0
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-08-12 15:32:48 +0300

    refactor: merging

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -107,7 +107,10 @@ const Navbar = () => {
                         </button>
                     </div>
                     <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
-                        <div className='flex flex-shrink-0 items-center'>
+                        <Link
+                            className='flex flex-shrink-0 items-center'
+                            href='/'
+                        >
                             <Image
                                 className='hover:cursor-pointer'
                                 src='/images/logo.png'
@@ -115,7 +118,7 @@ const Navbar = () => {
                                 height={40}
                                 width={100}
                             />
-                        </div>
+                        </Link>
                     </div>
                     <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
                         <div className='hidden sm:ml-6 sm:block'>
