91d9489ca054b2cfd638d03bc7310671ec9ae077
jangismarifet <jangismarifet@gmail.com>
2022-08-15 15:02:41 +0300

    test(jest): tests dont pass
    tests don't pass because of Firebase. It says INTERNAL ASSERTION FAILED: Expected a class definition
    export const auth = firebase.auth();

--- src/components/SignInUp/SignInUp.test.jsx
+++ src/components/SignInUp/SignInUp.test.jsx
@@ -2,9 +2,9 @@
 
 import renderer from "react-test-renderer";
 
-import Signupin from "./SignInUp";
+import SignInUp from "./SignInUp";
 
 it("renders correctly", () => {
-    const tree = renderer.create(<Signupin title='Sign up' />).toJSON();
+    const tree = renderer.create(<SignInUp title='Sign up' />).toJSON();
     expect(tree).toMatchSnapshot();
 });
