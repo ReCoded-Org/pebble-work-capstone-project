3bc340845f2b77282d92ba8fddf59148028756be
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-08-19 18:45:49 +0300

    feat: translations added

--- src/components/SignInUp/SignInUp.jsx
+++ src/components/SignInUp/SignInUp.jsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useTranslation } from "next-i18next";
 import React, { useEffect, useState } from "react";
 const Signupin = ({ title }) => {
+    const { t } = useTranslation("common");
     const [domLoaded, setDomLoaded] = useState(false);
     useEffect(() => {
         setDomLoaded(true);
@@ -10,7 +12,7 @@ const Signupin = ({ title }) => {
     return (
         <>
             {domLoaded && (
-                <form className='m-5  flex h-full flex-col  items-center  justify-around   lg:flex-row'>
+                <form className='signInUp m-5 flex h-full flex-col  items-center  justify-around   lg:flex-row'>
                     <div className='h-100 flex w-96 flex-col items-center justify-center text-center 2xl:scale-150 '>
                         <Image
                             src='/svg/signupinMan.svg'
@@ -21,7 +23,9 @@ const Signupin = ({ title }) => {
                     </div>
                     <div className='flex w-96 flex-col items-center justify-center py-1 text-center lg:items-start  2xl:scale-150'>
                         <h1 className='py-3 text-3xl font-semibold md:flex'>
-                            {title}
+                            {title === "Sign in"
+                                ? t("common.nav.signIn")
+                                : t("common.nav.signUp")}
                         </h1>
                         <div className='w-80'>
                             {title === "Sign up" && (
@@ -31,7 +35,7 @@ const Signupin = ({ title }) => {
                                             className='focus:shadow-outline mx-px  w-40 appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:ring-secondary-200'
                                             id='name'
                                             type='text'
-                                            placeholder='Name'
+                                            placeholder={t("signInUpPage.name")}
                                         />
                                     </div>
                                     <div>
@@ -39,7 +43,9 @@ const Signupin = ({ title }) => {
                                             className='focus:shadow-outline  w-40 appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:ring-secondary-200'
                                             id='surname'
                                             type='text'
-                                            placeholder='Surname'
+                                            placeholder={t(
+                                                "signInUpPage.surname"
+                                            )}
                                         />
                                     </div>
                                 </div>
@@ -49,7 +55,7 @@ const Signupin = ({ title }) => {
                                     className='focus:shadow-outline w-80 appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:ring-secondary-200'
                                     id='email'
                                     type='email'
-                                    placeholder='Email address'
+                                    placeholder={t("signInUpPage.mail")}
                                 />
                             </div>
                             <div className='ml-px py-1'>
@@ -57,7 +63,7 @@ const Signupin = ({ title }) => {
                                     className='focus:shadow-outline w-80 appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:ring-secondary-200'
                                     id='password'
                                     type='password'
-                                    placeholder='Password'
+                                    placeholder={t("signInUpPage.password")}
                                 />
                             </div>
                             <p className='hidden text-xs italic text-red-500'>
@@ -65,30 +71,28 @@ const Signupin = ({ title }) => {
                             </p>
                             {title === "Sign up" ? (
                                 <p className='signin m-1 flex text-xs italic text-gray-700'>
-                                    Already have an account?
+                                    {t("signInUpPage.haveAccount")}&nbsp;
                                     <Link href='/signin'>
-                                        <div className='cursor-pointer border-b border-primary-200 pl-1 text-primary-200'>
-                                            {title === "Sign up"
-                                                ? "Sign in"
-                                                : "/Sign up"}
+                                        <div className='cursor-pointer border-b border-primary-200  text-primary-200'>
+                                            {t("common.nav.signIn")}
                                         </div>
                                     </Link>
                                 </p>
                             ) : (
                                 <p className='signin m-1 flex text-xs italic text-gray-700'>
-                                    Don&apos;t have an account? &nbsp;
+                                    {t("signInUpPage.dontHaveAccount")}&nbsp;
                                     <Link href='/signup'>
                                         <div className='cursor-pointer border-b border-primary-200 text-primary-200'>
-                                            {title === "Sign up"
-                                                ? "Sign in"
-                                                : "Sign up"}
+                                            {t("common.nav.signUp")}
                                         </div>
                                     </Link>
                                 </p>
                             )}
                             <div className=' py-1'>
                                 <button className='w-80 rounded bg-primary-200 px-2 py-1 md:w-auto '>
-                                    {title}
+                                    {title === "Sign up"
+                                        ? t("common.nav.signUp")
+                                        : t("common.nav.signIn")}
                                 </button>
                             </div>
                             <div className='flex flex-row items-center justify-center py-1'>
@@ -106,8 +110,8 @@ const Signupin = ({ title }) => {
                                     height={2}
                                 />
                             </div>
-                            <div className='flex justify-center py-1'>
-                                <button className=' flex w-96 flex-row items-center justify-center rounded border-2 border-b-4  border-black bg-transparent py-1 font-normal text-black hover:border-primary-200 hover:text-primary-200'>
+                            <div className=' flex justify-center py-1'>
+                                <button className='google flex w-96 flex-row items-center justify-center rounded border-2 border-b-4  border-black bg-transparent py-1 font-normal text-black hover:border-primary-200 hover:text-primary-200'>
                                     <Image
                                         className='px-1'
                                         src='/svg/google.svg'
@@ -115,7 +119,7 @@ const Signupin = ({ title }) => {
                                         height={30}
                                         width={30}
                                     />
-                                    Continue with Google
+                                    {t("signInUpPage.google")}
                                 </button>
                             </div>
                             <div className='flex justify-center py-1'>
@@ -127,7 +131,7 @@ const Signupin = ({ title }) => {
                                         height={28}
                                         width={28}
                                     />
-                                    Continue with Twitter
+                                    {t("signInUpPage.twitter")}
                                 </button>
                             </div>
                         </div>
