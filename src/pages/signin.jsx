6eee42c9cfb3c5e2353376ed9928a39d8c58077c
jangismarifet <jangismarifet@gmail.com>
2022-08-19 15:09:16 +0300

    test(passing tests): resolved conflicts lead to new test issues

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 import Layout from "@/components/layout/Layout";
-<<<<<<< HEAD:src/pages/signin/index.jsx
 import SignInUp from "@/components/SignInUp/SignInUp";
-=======
-import SignInUp from "@/components/SignInUp";
->>>>>>> efdd82c824a118a1277e834781a1cd1c436a1313:src/pages/signin.jsx
 
 const Signupinpage = () => {
     return (
