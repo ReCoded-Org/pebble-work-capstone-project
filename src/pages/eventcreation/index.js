ae282511ba7def815d714c27c33b464a03515683
oznuren <95101906+oznuren@users.noreply.github.com>
2022-08-23 18:24:11 +0300

    feat: added the translation for about page and the event page
    #77

--- src/pages/eventcreation/index.js
+++ src/pages/eventcreation/index.js
@@ -1,3 +1,4 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import React from "react";
 
 import EventCreation from "@/components/EventCreation";
@@ -12,5 +13,14 @@ const Eventcreation = () => {
         </div>
     );
 };
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common"])),
+            // Will be passed to the page component as props
+        },
+    };
+}
 
 export default Eventcreation;
