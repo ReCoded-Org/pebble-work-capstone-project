4491c1d0ff7064bffa00e9713ab7dee17892676f
jangismarifet <jangismarifet@gmail.com>
2022-08-25 13:26:16 +0300

    feat(navbar permanent signin): stored signin context in the localstorage

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,10 +1,13 @@
 import { createContext, useEffect,useState } from "react";
 
+
 const AuthContext = createContext({});
+// localStorage.setItem(JSON.stringify())
 
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState(null);
     useEffect(() => {
+        console.log("useeffect auth", auth);
         if (auth === null) return;
         localStorage.setItem("user", JSON.stringify(auth));
     }, [auth]);
@@ -15,6 +18,8 @@ export const AuthProvider = ({ children }) => {
                 : JSON.parse(localStorage.getItem("user"));
         setAuth(data);
     }, []);
+
+
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
             {children}
