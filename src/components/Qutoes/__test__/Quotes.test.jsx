1d1b7d674bf1e68ae5dc32e88e13ee571ddee1f3
DKatuk <dilarakatuk@gmail.com>
2022-08-28 13:03:38 +0300

    feat: created your events page- i18next translations-quotes components

---
+++ src/components/Qutoes/__test__/Quotes.test.jsx
@@ -0,0 +1,8 @@
+import renderer from "react-test-renderer";
+
+import Quotes from "../Quotes";
+
+it("renders correctly", () => {
+    const tree = renderer.create(<Quotes />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
