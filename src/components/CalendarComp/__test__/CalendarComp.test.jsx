daa770dadc99cae5a5cb7b7c41f51f029fcd5ca7
oznuren <95101906+oznuren@users.noreply.github.com>
2022-08-28 15:57:50 +0300

    test: test fix

--- src/components/CalendarComp/__test__/CalendarComp.test.jsx
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-
-import CalendarComp from "../CalendarComp";
-
-it("renders correctly", () => {
-    const tree = renderer.create(<CalendarComp />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
