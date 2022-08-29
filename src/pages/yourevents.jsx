a872bde2cf326efd8dfdab44d395fabd55a32d20
Omer <omerfarukatas96@gmail.com>
2022-08-29 14:22:20 +0300

    refactor(yourevents): trying to fetch user's events and render
    We decided to take the info (events attended by user and events created by user) from the server
    instead of from auth, which we successfully did. but we cant render it
    
    re #124 re #156

--- src/pages/yourevents.jsx
+++ src/pages/yourevents.jsx
@@ -9,40 +9,92 @@ import Layout from "@/components/layout/Layout";
 import Pagination from "@/components/Pagination/Pagination";
 import Quotes from "@/components/Qutoes";
 
+import axios from "@/api/axios";
+
 export async function getServerSideProps({ locale }) {
-    const events = await fetch(
-        `https://pebble-work.herokuapp.com/api/event/`
-    ).then((r) => r.json());
+
     return {
         props: {
             ...(await serverSideTranslations(locale, ["common"])),
-            events,
         },
     };
 }
 
-const YourEvents = ({ events }) => {
-    // RENDERING UPCOMING EVENTS
+const YourEvents = () => {
+    // RENDERING THE EVENTS: CREATED BY USER AND WILL BE ATTENDED BY USER
     const { t } = useTranslation("common");
     let today = new Date();
     let formattedToday = format(today, "yyyy-MM-dd");
+    
+    let futureEvents = []
+    let createdFutureEvents = []
 
-    const futureEvents = [];
-    events.map((e) => {
-        if (e.date.split("T")[0] >= formattedToday) {
-            futureEvents.push(e);
-        }
-    });
+    const updateEvents = async () => {
+        const user = await axios({
+            method: "get",
+            url:"/api/user/",
+            withCredentials:true
+        })
+        // console.log(user);
+        user.data.followedEvents.map((e) => {
+            if (e.date.split("T")[0] >= formattedToday) {
+                futureEvents.push(e);
+            }
+        })
+        // console.log("FUTURE EVENTS", futureEvents);
+        user.data.createdEvents.map((e) => {
+            if (e.date.split("T")[0] >= formattedToday) {
+                createdFutureEvents.push(e);
+            }
+        })
+        // console.log(createdFutureEvents);
+    }
+    
+    updateEvents();
+    console.log("OUTSIDE FN: FUTURE EVENTS", futureEvents);
+
+        // const { auth } = useAuth();
+        // console.log("auth",auth)
+        
+        // let followedEvents =auth?.followedEvents;
+        // let createdEvents =auth?.createdEvents;
+    
+        // //BRINGING EVENTS THE USER WILL ATTEND
+        // const futureEvents = [];
+        // followedEvents.map((e) => {
+        //     if (e.date.split("T")[0] >= formattedToday) {
+        //         futureEvents.push(e);
+        //     }
+        // });
+    
+        // //BRINGING EVENTS CREATED BY USER
+        // const createdFutureEvents= [];
+        // createdEvents.map((e) => {
+        //     if (e.date.split("T")[0] >= formattedToday) {
+        //         createdFutureEvents.push(e);
+        //     }
+        // });
 
     //PAGINATION
     const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(10);
+    const [postsPerPage] = useState(5);
 
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
     const currentPosts = futureEvents.slice(indexOfFirstPost, indexOfLastPost);
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+    console.log("CURRENT POSTS", currentPosts);
+    
+    //PAGINATION FOR EVENTS CREATED BY USER
+    const [currentPageForCreated, setCurrentPageForCreated] = useState(1);
+    const [postsPerPageForCreated] = useState(5);
+
+    const indexOfLastPostForCreated = currentPageForCreated * postsPerPageForCreated;
+    const indexOfFirstPostForCreated = indexOfLastPostForCreated - postsPerPageForCreated;
+    const currentPostsForCreated = createdFutureEvents.slice(indexOfFirstPostForCreated, indexOfLastPostForCreated);
+    const paginateForCreated = (pageNumber) => setCurrentPageForCreated(pageNumber);
+    
     //JOIN-LEAVE EVENTS
     const [isJoined, setIsJoined] = useState({});
     function handleJoinClick(id) {
@@ -56,14 +108,15 @@ const YourEvents = ({ events }) => {
     return (
         <Layout>
             <Quotes />
+            {/* RENDERING UPCOMING EVENTS */}
             <div className='justify center my-6 flex flex-col items-center'>
                 <h1 className='text-2xl font-bold'>
                     {t("yourEvents.yourUpcomingEvents")}
                 </h1>
             </div>
-            <hr />
+            {/* <hr /> */}
             <div className='mx-4 sm:mx-44'>
-                {events.length > 0 ? (
+                {futureEvents.length > 0 ? (
                     <div>
                         <div className='my-10'>
                             <EventCards
@@ -75,7 +128,7 @@ const YourEvents = ({ events }) => {
                         <div className='mb-10'>
                             <Pagination
                                 postsPerPage={postsPerPage}
-                                totalPosts={events.length}
+                                totalPosts={futureEvents.length}
                                 paginate={paginate}
                             />
                         </div>
@@ -86,6 +139,42 @@ const YourEvents = ({ events }) => {
                         <Link href='/events'>
                             <p className='text-secondary-300 underline hover:cursor-pointer'>
                                 {t("yourEvents.exploreEvents")}
+                            </p>
+                        </Link>
+                    </div>
+                )}
+            </div>
+            {/* RENDERING EVENTS CREATED BY USER */}
+            <hr />
+            <div className='justify center my-6 flex flex-col items-center'>
+                <h1 className='text-2xl font-bold'>
+                    {t("yourEvents.eventsCreatedByYou")}
+                </h1>
+            </div>
+            <div className='mx-4 sm:mx-44'>
+                {createdFutureEvents.length > 0 ? (
+                    <div>
+                        <div className='my-10'>
+                            <EventCards
+                                events={currentPostsForCreated}
+                                isJoined={isJoined}
+                                handleJoinClick={handleJoinClick}
+                            />
+                        </div>
+                        <div className='mb-10'>
+                            <Pagination
+                                postsPerPage={postsPerPageForCreated}
+                                totalPosts={createdFutureEvents.length}
+                                paginate={paginateForCreated}
+                            />
+                        </div>
+                    </div>
+                ) : (
+                    <div className='mt-14 mb-20 flex flex-col items-center text-center text-xl sm:my-36 sm:text-2xl'>
+                        <p>{t("yourEvents.notCreated")}</p>
+                        <Link href='/eventcreation'>
+                            <p className='text-secondary-300 underline hover:cursor-pointer'>
+                                {t("yourEvents.createEvents")}
                             </p>
                         </Link>
                     </div>
