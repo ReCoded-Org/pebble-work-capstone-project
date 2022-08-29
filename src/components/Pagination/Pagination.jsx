6360e8bce826ba72826081c55505c6f1f8cb8455
oznuren <95101906+oznuren@users.noreply.github.com>
2022-08-29 16:17:41 +0300

    fix: scroll to top when clicked
    #127

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -5,18 +5,25 @@ import Button from "../Button";
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
     const pageNumbers = [];
 
+    const componentDidMount = () => {
+        window.scrollTo(0, 0);
+    };
+
     for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
         pageNumbers.push(i);
     }
 
     return (
-        <nav>
+        <nav className=''>
             <ul className='flex justify-center overflow-hidden'>
                 {pageNumbers.map((number) => (
                     <li key={number}>
                         <Button
                             customStyle='m-2 border-solid border-black hover:cursor-pointer focus:bg-secondary-100'
-                            onClick={() => paginate(number)}
+                            onClick={() => {
+                                paginate(number);
+                                componentDidMount();
+                            }}
                             label={number}
                         />
                     </li>
